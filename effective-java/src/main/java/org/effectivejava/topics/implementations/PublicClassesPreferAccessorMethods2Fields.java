b902a8c75db8a329cd388ff85eee65c8f54d9072
Danya Minakov <dany.minakov@gmail.com>
2023-05-09 11:34:56 +0300

    Item 16: In public classes, use accessor methods, not public fields

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/PublicClassesPreferAccessorMethods2Fields.java
@@ -0,0 +1,44 @@
+package org.effectivejava.topics.implementations;
+
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.util.List;
+
+public class PublicClassesPreferAccessorMethods2Fields implements Item {
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_4;
+    }
+
+    @Override
+    public String getTheme() {
+        return "In public classes, use accessor methods, not public fields";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+                "Degenerate Public class below is bad, because: it doesn't offer the benefits of encapsulation:" +
+                    "you can't change the representation without changing the API" +
+                    "you can't take auxiliary action in case of accessing the field" +
+                    "you can't enforce invariants",
+                "Create getter and setters (if needed) and make the fields private if needed. Only if class is exposed to the client. " +
+                    "Sometimes there is no need in this if it is private or for the external usage",
+                "Bad examples in Java standard library: java.awt.Dimension and java.awt.Point",
+                "Accessor methods for final fields are also questionable, see last ex in an Item for example of invariants"
+        );
+    }
+
+    @Override
+    public void runExamples() {
+
+    }
+
+    // TAKEN FROM BOOK
+    // Degenerate classes like this should not be public!
+    public static class Point {
+        public double x;
+        public double y;
+    }
+}
