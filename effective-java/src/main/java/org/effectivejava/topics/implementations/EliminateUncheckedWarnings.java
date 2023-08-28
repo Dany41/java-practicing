1b6cae21587905b6506b29e43c3cab88d95a7234
Danya Minakov <dany.minakov@gmail.com>
2023-08-28 07:58:07 +0300

    Item 27: Eliminate unchecked warnings

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/EliminateUncheckedWarnings.java
@@ -0,0 +1,50 @@
+package org.effectivejava.topics.implementations;
+
+import com.google.auto.service.AutoService;
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AutoService(Item.class)
+public class EliminateUncheckedWarnings implements Item {
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_5;
+    }
+
+    @Override
+    public String getTheme() {
+        return "Eliminate unchecked warnings";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+                "when you work with generics - you can get lots of types of warnings: unchecked cast warnings, " +
+                        "unchecked method invocation warnings, unchecked parametrized vararg type warnings, " +
+                        "unchecked conversion warnings",
+                "'<>' - diamond operator",
+                "eliminate every unchecked warning that you can",
+                "elimination of all warnings - means you will not get a ClassCastException at runtime",
+                "if you can't eliminate a warning, but you can prove that the code that provoked the warning is typesafe, " +
+                        "the (and only then) suppress the warning with an @SuppressWarnings(\"unchecked\") annotation",
+                "annotation @SuppressWarnings can be used on every declaration",
+                "always use it on the smallest scope possible, look at toArray method in ArrayList",
+                "every time you use a @SuppressWarnings(\"unchecked\") annotation, add a comment saying why it is safe to do so"
+        );
+    }
+
+    @Override
+    public void runExamples() {
+
+    }
+
+    @Override
+    public List<Class<?>> examplesInCode() {
+        return List.of(
+                ArrayList.class // toArray method
+        );
+    }
+}
