de90de7cf7b9b33c1ebff0c997b8f61556e82c5d
Daniil Minakov <dany.minakov@gmail.com>
2023-09-24 08:51:40 +0300

    Item 55: Return optionals judiciously

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/ReturnOptionalsJudiciously.java
@@ -0,0 +1,48 @@
+package org.effectivejava.topics.implementations;
+
+import com.google.auto.service.AutoService;
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.util.List;
+
+@AutoService(Item.class)
+public class ReturnOptionalsJudiciously implements Item {
+
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_8;
+    }
+
+    @Override
+    public String getTheme() {
+        return "Return optionals judiciously";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+                "never return a null value from an Optional-returning method",
+                "optionals are similar in spirit to checked exceptions",
+                "container types, including collections, maps, streams, arrays, and optionals should not be wrapped " +
+                        "in optionals",
+                "you should declare a method to return Optional<T> if it might not be able to return a result and " +
+                        "clients wil have to perform special processing if no result is returned",
+                "allocating optional objects and then unwrapping it is costly for performance-critical situations",
+                "you should never return an optional of a boxed primitive type (use OptionalInt, etc classes)",
+                "it is almost never appropriate to use an optional as a key, value, or element in a collection or array"
+        );
+    }
+
+    @Override
+    public void runExamples() {
+
+    }
+
+    @Override
+    public List<Class<?>> examplesInCode() {
+        return List.of(
+            ProcessHandle.class // method arguments returns array in optional
+        );
+    }
+}
