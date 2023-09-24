3b441a3d631992cb833645a20349ebc320fb55de
Daniil Minakov <dany.minakov@gmail.com>
2023-09-24 08:12:11 +0300

    Item 54: Return empty collections or arrays, not nulls

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/ReturnEmptyCollectionsOrArraysNotNulls.java
@@ -0,0 +1,43 @@
+package org.effectivejava.topics.implementations;
+
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.util.List;
+
+public class ReturnEmptyCollectionsOrArraysNotNulls implements Item {
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_8;
+    }
+
+    @Override
+    public String getTheme() {
+        return "Return empty collections or arrays, not nulls";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+            "never return null in place of an empty array or collection",
+            "there is argument that returning empty collection requires allocation and so - it has bad performance, " +
+                    "so null should be returned",
+            "it is unlikely that allocation affect your performance",
+            "also, you can use immutable empty collections or arrays, for example Collections.emptyList()",
+            "never preallocate the array, it harms performance, meaning never do this - k.toArray(new Foo[list.size()])",
+            "also returning null force clients of code always to check for null"
+        );
+    }
+
+    @Override
+    public void runExamples() {
+
+    }
+
+    @Override
+    public List<Class<?>> examplesInCode() {
+        return List.of(
+
+        );
+    }
+}
