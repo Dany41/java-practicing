1e42b599b10b65a60c9a01664b2ba7a1869c110e
Daniil Minakov <dany.minakov@gmail.com>
2023-09-26 09:22:36 +0300

    Item 60: Avoid float and double if exact answers required

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/AvoidFloatAndDoubleIfExactAnswersRequired.java
@@ -0,0 +1,66 @@
+package org.effectivejava.topics.implementations;
+
+import com.google.auto.service.AutoService;
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@AutoService(Item.class)
+public class AvoidFloatAndDoubleIfExactAnswersRequired implements Item {
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_9;
+    }
+
+    @Override
+    public String getTheme() {
+        return "Avoid float and double if exact answers required";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+                "the float and double types are designed for scientific and engineering calculations",
+                "they perform binary floating-point arithmetic",
+                "the float and double types are particularly ill-suited for monetary calculations, because it is " +
+                        "impossible to represent 0.1 (or any other negative power of ten)",
+                "always use BigDecimal, int, ot long for monetary calculations"
+        );
+    }
+
+    @Override
+    public void runExamples() {
+        System.out.println(1.03 - 0.42);
+        System.out.println(1.00 - 9 * 0.10);
+
+        double funds = 1.00;
+        int itemsBought = 0;
+        for (double price = 0.10; funds >= price; price += 0.10) {
+            funds -= price;
+            itemsBought++;
+        }
+        System.out.println(itemsBought + " items bought.");
+        System.out.println("Change: $" + funds);
+
+        final BigDecimal TEN_CENTS = new BigDecimal(".10");
+        int itemsBought2 = 0;
+        BigDecimal funds2 = new BigDecimal("1.00");
+        for (BigDecimal price = TEN_CENTS;
+             funds2.compareTo(price) >= 0;
+             price = price.add(TEN_CENTS)) {
+            funds2 = funds2.subtract(price);
+            itemsBought2++;
+        }
+        System.out.println(itemsBought2 + " items bought.");
+        System.out.println("Money left over: $" + funds2);
+    }
+
+    @Override
+    public List<Class<?>> examplesInCode() {
+        return List.of(
+
+        );
+    }
+}
