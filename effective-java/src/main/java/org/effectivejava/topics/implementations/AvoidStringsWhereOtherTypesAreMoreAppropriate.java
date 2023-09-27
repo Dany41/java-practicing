6fcb88c30cc7b1a014f3518c579e3db21f11b712
Daniil Minakov <dany.minakov@gmail.com>
2023-09-27 09:15:20 +0300

    Item 62: Avoid strings where other types are more appropriate

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/AvoidStringsWhereOtherTypesAreMoreAppropriate.java
@@ -0,0 +1,70 @@
+package org.effectivejava.topics.implementations;
+
+import com.google.auto.service.AutoService;
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.util.List;
+
+@AutoService(Item.class)
+public class AvoidStringsWhereOtherTypesAreMoreAppropriate implements Item {
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_9;
+    }
+
+    @Override
+    public String getTheme() {
+        return "Avoid strings where other types are more appropriate";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+                "Strings are poor substitutes for other value types",
+                "Strings are poor substitutes for enum types",
+                "Strings are poor substitutes for aggregate types",
+                "Strings are poor substitutes for capabilities",
+                "avoid representing objects as strings when better data types exist or can be written"
+        );
+    }
+
+    @Override
+    public void runExamples() {
+        // Inappropriate use of string as aggregate type
+//        String compoundKey = className + "#" + i.next();
+    }
+
+    @Override
+    public List<Class<?>> examplesInCode() {
+        return List.of(
+
+        );
+    }
+
+    // Broken - inappropriate use of string as capability!
+    private static class ThreadLocal {
+        private ThreadLocal() { } // Noninstantiable
+        // Sets the current thread's value for the named variable.
+        public static void set(String key, Object value) {};
+        // Returns the current thread's value for the named variable.
+        public static Object get(String key) {
+            return null;
+        }
+    }
+
+    private static class ThreadLocalOk {
+        private ThreadLocalOk() { } // Noninstantiable
+        public static class Key { // (Capability)
+            Key() { }
+        }
+        // Generates a unique, unforgeable key
+        public static Key getKey() {
+            return new Key();
+        }
+        public static void set(Key key, Object value) {};
+        public static Object get(Key key) {
+            return null;
+        }
+    }
+}
