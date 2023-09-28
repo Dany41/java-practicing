fb3593a927edf00d4dc20ad50938256948bd61c9
Daniil Minakov <dany.minakov@gmail.com>
2023-09-28 08:40:16 +0300

    Item 66: Use native methods judiciously

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/UseNativeMethodsJudiciously.java
@@ -0,0 +1,48 @@
+package org.effectivejava.topics.implementations;
+
+import com.google.auto.service.AutoService;
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.util.List;
+
+@AutoService(Item.class)
+public class UseNativeMethodsJudiciously implements Item {
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_9;
+    }
+
+    @Override
+    public String getTheme() {
+        return "Use native methods judiciously";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+                "Java Native Interface (JNI) allows Java programs to call native methods, which are written in native " +
+                        "programming languages such as C or C++",
+                """
+                        there are three main reasons for using native methods:
+                            - they provide access to platform-specific facilities such as registries
+                            - they provide access to existing libraries of native code, including legacy libraries that
+                                provide access to legacy data
+                            - they are used to write performance-critical parts of applications in native languages for
+                                improved performance""",
+                "it is rarely advisable to use native methods for improved performance"
+        );
+    }
+
+    @Override
+    public void runExamples() {
+
+    }
+
+    @Override
+    public List<Class<?>> examplesInCode() {
+        return List.of(
+
+        );
+    }
+}
