0757e6636d9a3e30394fbf1d9031a51a0b8b8010
Daniil Minakov <dany.minakov@gmail.com>
2023-09-28 08:31:54 +0300

    Item 65: Prefer interfaces to reflection

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/PreferInterfacesToReflection.java
@@ -0,0 +1,46 @@
+package org.effectivejava.topics.implementations;
+
+import com.google.auto.service.AutoService;
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.util.List;
+
+@AutoService(Item.class)
+public class PreferInterfacesToReflection implements Item {
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_9;
+    }
+
+    @Override
+    public String getTheme() {
+        return "Prefer interfaces to reflection";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+                """
+                        reflection drawbacks:
+                            - you lose all the benefits of compile-time type checking
+                            - the code required to perform reflective access is clumsy and verbose
+                            - performance suffers""",
+                "you can obtain many of the benefits of reflection while incurring few of its costs by using it only in " +
+                        "a very limited form",
+                "you can create instances reflectively and access them normally via their interfaces or superclass"
+        );
+    }
+
+    @Override
+    public void runExamples() {
+
+    }
+
+    @Override
+    public List<Class<?>> examplesInCode() {
+        return List.of(
+
+        );
+    }
+}
