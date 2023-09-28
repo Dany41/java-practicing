f12c6f796567c5057157e07b18d04051049bca68
Daniil Minakov <dany.minakov@gmail.com>
2023-09-28 08:54:53 +0300

    Item 67: Optimize judiciously

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/OptimizeJudiciously.java
@@ -0,0 +1,48 @@
+package org.effectivejava.topics.implementations;
+
+import com.google.auto.service.AutoService;
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.awt.*;
+import java.util.List;
+
+@AutoService(Item.class)
+public class OptimizeJudiciously implements Item {
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_9;
+    }
+
+    @Override
+    public String getTheme() {
+        return "Optimize judiciously";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+                "strive to write good programs rather than fast ones",
+                "strive to avoid design decisions that limit performance",
+                "consider the performance consequences of your API design decisions",
+                "it is a very bad idea to warp an API to achieve good performance",
+                "measure performance before and after each attempted optimization",
+                """
+                        do not strive to write fast programs - strive to write good ones; speed will follow. But do
+                        think about performance while you're designing systems, especially while you're designing APIs,
+                        wire-level protocols, and persistent data formats"""
+        );
+    }
+
+    @Override
+    public void runExamples() {
+
+    }
+
+    @Override
+    public List<Class<?>> examplesInCode() {
+        return List.of(
+                Component.class // getSize()
+        );
+    }
+}
