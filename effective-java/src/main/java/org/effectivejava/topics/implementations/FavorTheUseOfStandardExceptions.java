a85632de36143346c935e961804739aaaf335995
Daniil Minakov <dany.minakov@gmail.com>
2023-09-30 19:26:18 +0300

    Item 72: Favor the use of standard exceptions

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/FavorTheUseOfStandardExceptions.java
@@ -0,0 +1,50 @@
+package org.effectivejava.topics.implementations;
+
+import com.google.auto.service.AutoService;
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.util.List;
+
+@AutoService(Item.class)
+public class FavorTheUseOfStandardExceptions implements Item {
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_10;
+    }
+
+    @Override
+    public String getTheme() {
+        return "Favor the use of standard exceptions";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+                "do not reuse Exception, RuntimeException, Throwable, or Error directly",
+                """
+                    here is the list of most commonly reused exceptions:
+                        IllegalArgumentException -> non-null parameter value is inappropriate
+                        IllegalStateException -> object state is inappropriate for method invocation
+                        NullPointerException -> parameter value is null where prohibited
+                        IndexOutOfBoundException -> index parameter value is out of range
+                        ConcurrentModificationException -> concurrent modification of an object has been detected where
+                                                            it is prohibited
+                        UnsupportedOperationException -> object does no support method""",
+                "throw IllegalStateException if no argument values would have worked, otherwise throw " +
+                        "IllegalArgumentException"
+        );
+    }
+
+    @Override
+    public void runExamples() {
+
+    }
+
+    @Override
+    public List<Class<?>> examplesInCode() {
+        return List.of(
+
+        );
+    }
+}
