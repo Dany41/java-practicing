efe6366ff24fcc51c00d9460595c4bc37668bef8
Daniil Minakov <dany.minakov@gmail.com>
2023-10-01 12:50:15 +0300

    small refactoring in FavorStaticMemberClassesOverNonStatic

--- effective-java/src/main/java/org/effectivejava/topics/implementations/FavorStaticMemberClassesOverNonStatic.java
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/FavorStaticMemberClassesOverNonStatic.java
@@ -31,9 +31,9 @@ public class FavorStaticMemberClassesOverNonStatic implements Item {
                 "Each instance of a nonstatic member class is implicitly associated with an enclosing instance of " +
                         "its containing class",
                 "Within instance methods of a nonstatic member class, you can invoke methods on the enclosing instance " +
-                        "or obtain a reference to the enclosing instance using the qualified this construct ??",
+                        "or obtain a reference to the enclosing instance using the qualified this construct (" +
+                        "<TypeName>.this)",
                 "If the instance of nested class can exist without instance of enclosing one - it must be static",
-                "But, it is possible to create an instance of a nonstatic member class without an enclosing instance",
                 "The association between a nonstatic member class instance and its enclosing instance is established " +
                         "when the member class instance is created and cannot be modified thereafter",
                 "It is possibly to set the association manually via: enclosingInstance.new MemberClass(args)",
@@ -58,5 +58,15 @@ public class FavorStaticMemberClassesOverNonStatic implements Item {
                 Set.class,
                 Map.class
         );
+    }
+
+    public static class A {
+
+    }
+
+    public class B {
+        void method() {
+            FavorStaticMemberClassesOverNonStatic.this.getChapter();
+        }
     }
 }
