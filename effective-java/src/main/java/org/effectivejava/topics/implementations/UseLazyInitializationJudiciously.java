df3179210a779e915966e81f09a73651b3ebc59a
Daniil Minakov <dany.minakov@gmail.com>
2023-10-03 08:43:26 +0300

    Item 83: Use lazy initialization judiciously

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/UseLazyInitializationJudiciously.java
@@ -0,0 +1,50 @@
+package org.effectivejava.topics.implementations;
+
+import com.google.auto.service.AutoService;
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.util.List;
+
+@AutoService(Item.class)
+public class UseLazyInitializationJudiciously implements Item {
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_11;
+    }
+
+    @Override
+    public String getTheme() {
+        return "Use lazy initialization judiciously";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+                "in the presence of multiple threads, lazy initialization is tricky. If two or more threads share a " +
+                        "lazily initialized field, it is critical that some form of synchronization is employed",
+                "under most circumstances, normal initialization is preferable to lazy initialization",
+                "if you use lazy initialization to break an initialization circularity, use a synchronized accessor",
+                "if you need to use lazy initialization for performance on a static field, use the lazy initialization" +
+                        "holder class idiom",
+                "if you need to use lazy initialization for performance on an instance field, use the double-check idiom",
+                "for double-check idiom it is critical for field to be volatile",
+                """
+                        summarizing lazily initialization techniques: instance fields - double-check idiom; static
+                        fields - lazy initialization holder class idiom; instance fields that can tolerate repeated
+                        initialization - single-check idiom"""
+        );
+    }
+
+    @Override
+    public void runExamples() {
+
+    }
+
+    @Override
+    public List<Class<?>> examplesInCode() {
+        return List.of(
+
+        );
+    }
+}
