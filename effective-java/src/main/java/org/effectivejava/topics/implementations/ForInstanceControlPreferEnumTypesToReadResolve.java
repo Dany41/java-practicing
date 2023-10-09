15a19bda400d969aeb9c39a8348ec3f45f079faa
Daniil Minakov <dany.minakov@gmail.com>
2023-10-09 08:13:14 +0300

    Item 89: For instance control, prefer enum types to readResolve

---
+++ effective-java/src/main/java/org/effectivejava/topics/implementations/ForInstanceControlPreferEnumTypesToReadResolve.java
@@ -0,0 +1,46 @@
+package org.effectivejava.topics.implementations;
+
+import com.google.auto.service.AutoService;
+import org.effectivejava.topics.abstractions.Item;
+import org.effectivejava.topics.helpers.Chapter;
+
+import java.util.List;
+
+@AutoService(Item.class)
+public class ForInstanceControlPreferEnumTypesToReadResolve implements Item {
+    @Override
+    public Chapter getChapter() {
+        return Chapter.CHAPTER_12;
+    }
+
+    @Override
+    public String getTheme() {
+        return "For instance control, prefer enum types to readResolve";
+    }
+
+    @Override
+    public List<String> getBulletPoints() {
+        return List.of(
+                "readResolve feature allows you to substitute another instance for the one created by readResolve (" +
+                        "useful for singletons)",
+                "if you depend on readResolve for instance control, all instance fields with object reference types " +
+                        "must be declared transient",
+                "the accessibility of readResolve is significant. In final class it should be private. If it is in " +
+                        "not final class - consider accessibility (if private - not visible for subclasses, etc)",
+                "with a special attack it is possible to create another instance of singleton object (if it is not " +
+                        "declared as enum)"
+        );
+    }
+
+    @Override
+    public void runExamples() {
+
+    }
+
+    @Override
+    public List<Class<?>> examplesInCode() {
+        return List.of(
+
+        );
+    }
+}
